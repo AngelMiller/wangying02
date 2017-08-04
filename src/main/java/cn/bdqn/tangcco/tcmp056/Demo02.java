f30f5b2ca1ad3e9314c4a5d4efe6456af5113ef1
AngelMiller <wy18201352841@163.com>
2017-08-04 09:38:42 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/Demo02.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by lenovo on 2017/8/4.
+ */
+public class Demo02 {
+    @Test
+    public void Demo02() {
+        System.out.println("HelloWorld");
+    }
+}
